8b394a6914bfbb03bb4122774ac795a9600d0462
YashwanthKumar <141126524+Yashwanthkumarkorakooti@users.noreply.github.com>
2025-08-12 14:37:55 +0530

    Time: 0 ms (100%), Space: 31.9 MB (22.82%) - LeetHub

---
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& grid, int r, int c, int n , int expVal){
+        if(r < 0 || r >= n || c < 0 || c >= n || grid[r][c] != expVal) return false;
+        if(expVal == pow(n,2) - 1) return true ;
+
+        bool ans1 = isValid(grid,r-1,c-2,n,expVal+1);
+        bool ans2 = isValid(grid,r-1,c+2,n,expVal+1);
+        bool ans3 = isValid(grid,r+1,c-2,n,expVal+1);
+        bool ans4 = isValid(grid,r+1,c+2,n,expVal+1);
+        bool ans5 = isValid(grid,r-2,c-1,n,expVal+1);
+        bool ans6 = isValid(grid,r-2,c+1,n,expVal+1);
+        bool ans7 = isValid(grid,r+2,c-1,n,expVal+1);
+        bool ans8 = isValid(grid,r+2,c+1,n,expVal+1);
+
+        return ans1 || ans2 || ans3 || ans4 || ans5|| ans6 || ans7 || ans8;
+    }   
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        return isValid(grid,0,0,grid.size(),0);
+    }
+};
