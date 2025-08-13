2bf81e014bbdd6229bed657c05c023d4ab1fa2ba
YashwanthKumar <141126524+Yashwanthkumarkorakooti@users.noreply.github.com>
2025-08-13 18:08:51 +0530

    Time: 9 ms (19.16%), Space: 23.8 MB (57.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,14 +4,25 @@ public:
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans ;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
         for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
-            }
-            else if(intervals[i][0] <= ans.back()[1]){
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            // if(ans.empty() || intervals[i][0] > ans.back()[1]){
+            //     ans.push_back(intervals[i]);
+            // }
+            // else if(intervals[i][0] <= ans.back()[1]){
+            //     ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            // }
+            if(end >= intervals[i][0]){
+                end = max(end,intervals[i][1]);
+            }else{
+                ans.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
+        ans.push_back({start,end});
         return ans;
     }
 };
